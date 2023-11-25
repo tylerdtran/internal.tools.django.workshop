0452c97a8d155af1207f0871ad3864539ee2de3c
Tyler Tran <tylerduytran@gmail.com>
2023-11-25 12:54:51 -0800

    Enabled Patch work

--- backend/sources/urls.py
+++ backend/sources/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import SourceListView
+from .views import SourceDetailView
 
 urlpatterns = [
     # path('sources', SourceListView.as_view(), name='source-list'),
     # path('sources/<int:pk>/', SourceDetailView.as_view(), name='source-detail'),
     path('sources', SourceListView.as_view()),
-
+    path('sources/<int:pk>/', SourceDetailView.as_view()),
 ]
--- backend/sources/views.py
+++ backend/sources/views.py
@@ -9,6 +9,25 @@ class SourceListView(generics.ListCreateAPIView):
     queryset = Source.objects.all()
     serializer_class = SourceSerializer
 
+
+class SourceDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Source.objects.all()
+    serializer_class = SourceSerializer
+
+# backend/sources/views.py
+
+# from django.shortcuts import get_object_or_404
+# from django.http import JsonResponse
+# from django.views import View
+# from .models import Source
+
+# class SourceDetailView(View):
+#     def delete(self, request, pk):
+#         source = get_object_or_404(Source, pk=pk)
+#         source.delete()
+#         return JsonResponse({'message': 'Source deleted successfully'}, status=204)
+
+
 # class SourceDetailView(generics.RetrieveUpdateDestroyAPIView):
 #     queryset = Source.objects.all()
 #     serializer_class = SourceSerializer
