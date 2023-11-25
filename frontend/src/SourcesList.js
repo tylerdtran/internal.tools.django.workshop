f8a3ed2a5c55fa610a50b9fc47f91fd195db34bb
Tyler Tran <tylerduytran@gmail.com>
2023-11-25 13:40:53 -0800

    Put now works, DB project requirement done

--- frontend/src/SourcesList.js
+++ frontend/src/SourcesList.js
@@ -4,6 +4,9 @@ import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow
 
 const SourcesList = () => {
   const [sources, setSources] = useState([]);
+  // const [updatedSource, setUpdatedSource] = useState({});
+  const [editingId, setEditingId] = useState(null);
+  const [updatedSource, setUpdatedSource] = useState({});
 
   useEffect(() => {
     const fetchSources = async () => {
@@ -29,14 +32,54 @@ const SourcesList = () => {
     }
   }; 
 
-  const handleUpdateSource = async (id, newName) => {
+  // const handleUpdateSource = async (id, updatedSource) => {
+  //   try {
+  //     const response = await axios.put(`http://localhost:5000/api/sources/${id}/`, updatedSource);
+  //     setSources(sources.map(source => source.id === id ? response.data : source));
+  //   } catch (error) {
+  //     console.error(`Error updating source with ID ${id}:`, error);
+  //   }
+  // };
+  // const handleUpdateSource = async (id, updatedSource) => {
+  //   // Check that all fields are filled in
+  //   if (!updatedSource.name || !updatedSource.organization || !updatedSource.email || !updatedSource.phone || !updatedSource.notes) {
+  //     alert('Please fill in all fields');
+  //     return;
+  //   }
+  
+  //   try {
+  //     await axios.put(`http://localhost:5000/api/sources/${id}/`, updatedSource);
+  //     // Refetch sources
+  //     const response = await axios.get('http://localhost:5000/api/sources/');
+  //     setSources(response.data);
+  //     // Reset editingId and updatedSource
+  //     setEditingId(null);
+  //     setUpdatedSource({});
+  //   } catch (error) {
+  //     console.error(`Error updating source with ID ${id}:`, error);
+  //   }
+  // };
+  const handleUpdateSource = async (id, updatedSource) => {
+    // Check that all fields are filled in
+    if (!updatedSource.name || !updatedSource.organization || !updatedSource.email || !updatedSource.phone || !updatedSource.notes) {
+      alert('Please fill in all fields');
+      return;
+    }
+  
     try {
-      const response = await axios.put(`http://localhost:5000/api/sources/${id}`, { name: newName });
-      setSources(sources.map(source => source.id === id ? response.data : source));
+      await axios.put(`http://localhost:5000/api/sources/${id}/`, updatedSource);
+      // Refetch sources
+      // await axios.get('http://localhost:5000/api/sources/${id}/');
+      // setSources(response.data);
+      window.location.reload();
+      // Reset editingId and updatedSource
+      setEditingId(null);
+      setUpdatedSource({});
     } catch (error) {
       console.error(`Error updating source with ID ${id}:`, error);
     }
   };
+
 
   return (
     <div>
@@ -57,15 +100,43 @@ const SourcesList = () => {
           <TableBody>
             {sources.map((source) => (
               <TableRow key={source.id}>
-                <TableCell>{source.name}</TableCell>
-                <TableCell>{source.organization}</TableCell>
-                <TableCell>{source.email}</TableCell>
-                <TableCell>{source.phone}</TableCell>
-                <TableCell>{source.notes}</TableCell>
-                <TableCell>
-                  <Button variant="contained" color="primary" onClick={() => handleUpdateSource(source.id, 'New Name')}>
-                    Update
+              {editingId === source.id ? (
+                <>
+                  <TableCell>
+                    <input value={updatedSource.name || ''} onChange={e => setUpdatedSource({ ...updatedSource, name: e.target.value })} />
+                  </TableCell>
+                  <TableCell>
+                    <input value={updatedSource.organization || ''} onChange={e => setUpdatedSource({ ...updatedSource, organization: e.target.value })} />
+                  </TableCell>
+                  <TableCell>
+                    <input value={updatedSource.email || ''} onChange={e => setUpdatedSource({ ...updatedSource, email: e.target.value })} />
+                  </TableCell>
+                  <TableCell>
+                    <input value={updatedSource.phone || ''} onChange={e => setUpdatedSource({ ...updatedSource, phone: e.target.value })} />
+                  </TableCell>
+                  <TableCell>
+                    <input value={updatedSource.notes || ''} onChange={e => setUpdatedSource({ ...updatedSource, notes: e.target.value })} />
+                  </TableCell>
+                </>
+              ) : (
+                <>
+                  <TableCell>{source.name}</TableCell>
+                  <TableCell>{source.organization}</TableCell>
+                  <TableCell>{source.email}</TableCell>
+                  <TableCell>{source.phone}</TableCell>
+                  <TableCell>{source.notes}</TableCell>
+                </>
+              )}
+              <TableCell>
+                {editingId === source.id ? (
+                  <Button variant="contained" color="primary" onClick={() => handleUpdateSource(source.id, updatedSource)}>
+                    Save
                   </Button>
+                ) : (
+                  <Button variant="contained" color="primary" onClick={() => setEditingId(source.id)}>
+                      Update
+                    </Button>
+                  )}
                 </TableCell>
                 <TableCell>
                   <Button variant="contained" color="secondary" onClick={() => handleDeleteSource(source.id)}>
@@ -82,3 +153,4 @@ const SourcesList = () => {
 };
 
 export default SourcesList;
+
